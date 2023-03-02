ad87761a4c45d85e12e5a36615eab521cccada23
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-03-02 08:01:19 +0100

    fix typo in eventlistener keyup

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
 }
 
 form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup' (e) => {
+form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
     handleSubmit(e);
   }
